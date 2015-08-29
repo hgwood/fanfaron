0d0d4615be1bf2853a53be054b624553e46e3aac
Hugo Wood <hx4.5@free.fr>
2015-08-29 22:39:52 +0200

    feat: added Example

--- src/main/java/fr/hgwood/fanfaron/Example.java
+++ src/main/java/fr/hgwood/fanfaron/Example.java
@@ -1,5 +1,15 @@
 package fr.hgwood.fanfaron;
 
-public class Example {
+import com.google.common.collect.ForwardingMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example extends ForwardingMap<String, Object> {
+
+    private final Map<String, Object> examples = new HashMap<>();
+
+    @Override protected Map<String, Object> delegate() {
+        return examples;
+    }
 }
