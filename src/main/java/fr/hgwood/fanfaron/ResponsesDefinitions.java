a622a425f7b0660ddbe4cafc371deca5f0f47050
Hugo Wood <hx4.5@free.fr>
2015-08-29 22:55:31 +0200

    feat: added ResponsesDefinitions

--- src/main/java/fr/hgwood/fanfaron/ResponsesDefinitions.java
+++ src/main/java/fr/hgwood/fanfaron/ResponsesDefinitions.java
@@ -1,5 +1,16 @@
 package fr.hgwood.fanfaron;
 
-public class ResponsesDefinitions {
+import com.google.common.collect.ForwardingMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponsesDefinitions extends ForwardingMap<String, Response> {
+
+    private final Map<String, Response> responses = new HashMap<>();
+
+    @Override protected Map<String, Response> delegate() {
+        return responses;
+    }
 
 }
