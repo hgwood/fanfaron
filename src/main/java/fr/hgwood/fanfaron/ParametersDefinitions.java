71d8eeab555d468a19c1666570568e6d240ec0f5
Hugo Wood <hx4.5@free.fr>
2015-08-29 22:55:47 +0200

    feat: added ParametersDefinitions

--- src/main/java/fr/hgwood/fanfaron/ParametersDefinitions.java
+++ src/main/java/fr/hgwood/fanfaron/ParametersDefinitions.java
@@ -1,5 +1,16 @@
 package fr.hgwood.fanfaron;
 
-public class ParametersDefinitions {
+import com.google.common.collect.ForwardingMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParametersDefinitions extends ForwardingMap<String, Parameter> {
+
+    private final Map<String, Parameter> parameters = new HashMap<>();
+
+    @Override protected Map<String, Parameter> delegate() {
+        return parameters;
+    }
 
 }
